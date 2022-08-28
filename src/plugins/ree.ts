bc3856ed18ea02af6311454d476e82e26a913590
raluvy95 <ralucadoge069@gmail.com>
2022-08-28 19:44:14 +0300

    replaced includes with startswith

--- src/plugins/ree.ts
+++ src/plugins/ree.ts
@@ -3,7 +3,7 @@ import { send } from "@utils/send.ts";
 
 export async function ree(client: BotWithCache<Bot>, message: Message) {
     if (!config.plugins.ree) return;
-    if (message.content.includes("ree")) {
+    if (message.content.startsWith("ree")) {
         await send(client, message.channelId, "REEEEEEEEEEEEEEEEEEEE")
     }
 } 
