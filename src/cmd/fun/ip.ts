93b7a23a4569bbfaa4590b842e485744e3936e84
raluvy95 <ralucadoge069@gmail.com>
2022-08-28 18:38:02 +0300

    Added ip command

---
+++ src/cmd/fun/ip.ts
@@ -0,0 +1,25 @@
+import { Bot, BotWithCache, Message } from "@deps";
+import { addCommand, JollyCommand } from "@classes/command.ts";
+import { send } from "@utils/send.ts";
+import { JollyEmbed } from "../../classes/embed.ts";
+import { isIP } from "https://deno.land/x/isIP@1.0.0/mod.ts";
+
+class IP extends JollyCommand {
+    constructor() {
+        super("ip", "fun")
+    }
+
+    override async run(message: Message, args: string[], client: BotWithCache<Bot>) {
+        if (!args[0]) return send(client, message.channelId, "Give me the IP.")
+        if (isIP(args[0]) == 0) return send(client, message.channelId, "That's not an IP")
+        const jj = await fetch(`https://ipinfo.io/${args[0]}/geo`)
+        const info = await jj.json()
+        const e = new JollyEmbed()
+            .setTitle(`IP: ${info.ip}`)
+            .setDesc(`City: ${info.city}\nRegion: ${info.region}\nCountry: ${info.country}\nLocation: ${info.loc}\nTimezone: ${info.timezone}\nPostal: ${info.postal}`)
+            .setColor(0xc45a23)
+        await send(client, message.channelId, e.build())
+    }
+}
+
+addCommand(new IP())
