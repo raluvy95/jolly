4f3d3ae4c25f8aba16bad59a93200dac483bfe6d
a boi here <41384430+some-boi@users.noreply.github.com>
2022-08-29 22:43:06 +0100

    ,nb

--- src/plugins/bumpReminder.ts
+++ src/plugins/bumpReminder.ts
@@ -4,9 +4,9 @@ import { send } from "@utils/send.ts";
 export const bumpReminder = async (client: BotWithCache<Bot>, message: Message): Promise<void> => {
     if (config.plugins.bump.enable && message.interaction?.name == "bump"
         && message.authorId === 302050872383242240n) {
-        await send(client, message.channelId, `I will remind you to bump again in two hours!`)
+        send(client, message.channelId, `I will remind you to bump again in two hours!`)
         setTimeout(async () => {
-            return await send(client, message.channelId, {
+            return send(client, message.channelId, {
                 content: "Hey <@&$ID>, reminder to `/bump` again!".replace("$ID", config.plugins.bump.roleID),
                 allowedMentions: {
                     roles: [BigInt(config.plugins.bump.roleID)]
