ecc2d2c4b35e17d19297b76950869390a8b2d6e6
a boi here <41384430+some-boi@users.noreply.github.com>
2022-08-29 23:50:27 +0100

    Update avatarURL.ts

--- src/utils/avatarURL.ts
+++ src/utils/avatarURL.ts
@@ -1,7 +1,7 @@
 import { Bot, BotWithCache, User } from "@deps";
 
 export async function avatarURL(bot: BotWithCache<Bot>, user: bigint | User): Promise<string> {
-    const boi = typeof user == "bigint" ? bot.users.get(user) || await bot.helpers.getUser(user) : user;
+    const u = typeof user == "bigint" ? bot.users.get(user) || await bot.helpers.getUser(user) : user;
     if (!u) throw new Error("Cannot find that user")
-    return bot.helpers.avatarURL(user.id, user.discriminator, { avatar: user.avatar, format: "png" })
+    return bot.helpers.avatarURL(u.id, u.discriminator, { avatar: u.avatar, format: "png" })
 }
