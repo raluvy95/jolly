b8dbf7e37d833d409534616ad0874cd6bc2e6f77
a boi here <41384430+some-boi@users.noreply.github.com>
2022-08-29 22:09:40 +0100

    very epic better unwarnAll

--- src/cmd/moderation/unwarnall.ts
+++ src/cmd/moderation/unwarnall.ts
@@ -14,12 +14,7 @@ class UnWarnAll extends JollyCommand {
     override async run(message: Message, args: string[], client: BotWithCache<Bot>) {
         if (!args[0]) return await send(client, message.channelId, "Who do you want to remove all of their warnings?")
         const mentionUser = message.mentionedUserIds
-        let user: User | undefined
-        if (mentionUser.length >= 1) {
-            user = await findUser(client, mentionUser[0].toString())
-        } else {
-            user = await findUser(client, args[0])
-        }
+        const user = mentionUser.length >= 1 ? await findUser(client, mentionUser[0].toString()) : await findUser(client, args[0])
         if (!user) return await send(client, message.channelId, "That user is not found")
         warning.removeAll(user.id)
         return await send(client, message.channelId, "Successfully removed all warnings!")
