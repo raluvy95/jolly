e3f3504672d4aaa3b79bd43258cd141e679da070
a boi here <41384430+some-boi@users.noreply.github.com>
2022-08-29 22:47:05 +0100

    Update ree.ts

--- src/plugins/ree.ts
+++ src/plugins/ree.ts
@@ -4,6 +4,6 @@ import { send } from "@utils/send.ts";
 export async function ree(client: BotWithCache<Bot>, message: Message) {
     if (!config.plugins.ree) return;
     if (message.content.startsWith("ree")) {
-        await send(client, message.channelId, "REEEEEEEEEEEEEEEEEEEE")
+        send(client, message.channelId, "REEEEEEEEEEEEEEEEEEEE")
     }
 } 
