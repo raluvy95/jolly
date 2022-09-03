daf21dcfcb4aa1429b258e3a8a0e69068d53aa80
raluvy95 <ralucadoge069@gmail.com>
2022-09-03 22:07:22 +0300

    Bump to final version

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export * from "https://deno.land/x/discordeno@13.0.0-rc50/mod.ts";
-export * from "https://deno.land/x/discordeno@13.0.0-rc50/plugins/cache/mod.ts";
-export * from "https://deno.land/x/discordeno@13.0.0-rc50/plugins/permissions/mod.ts";
+export * from "https://deno.land/x/discordeno@13.0.0/mod.ts";
+export * from "https://deno.land/x/discordeno@13.0.0/plugins/cache/mod.ts";
+export { hasGuildPermissions, enablePermissionsPlugin } from "https://deno.land/x/discordeno@13.0.0/plugins/permissions/mod.ts";
 export * from "https://deno.land/std@0.152.0/fmt/colors.ts";
 export { default as config } from "./config.json" assert { type: "json" };
 export * from "https://deno.land/x/sqlite@v3.4.0/mod.ts";
--- src/utils/avatarURL.ts
+++ src/utils/avatarURL.ts
@@ -3,5 +3,5 @@ import { Bot, BotWithCache, User } from "@deps";
 export async function avatarURL(bot: BotWithCache<Bot>, user: bigint | User): Promise<string> {
     const u = typeof user == "bigint" ? bot.users.get(user) || await bot.helpers.getUser(user) : user;
     if (!u) throw new Error("Cannot find that user")
-    return bot.helpers.avatarURL(u.id, u.discriminator, { avatar: u.avatar, format: "png" })
+    return bot.helpers.getAvatarURL(u.id, u.discriminator, { avatar: u.avatar, format: "png" })
 }
