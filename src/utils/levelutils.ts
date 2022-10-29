a264b30167b24b88dee6dbc0d933c2bfa04dbf8b
raluvy95 <ralucadoge069@gmail.com>
2022-10-29 22:10:36 +0300

    Changed formula for required XP

--- src/utils/levelutils.ts
+++ src/utils/levelutils.ts
@@ -1,5 +1,7 @@
+// more accurate than formula used in video game :skull:
+
 export function XPrequiredToLvlUP(level: number) {
-    return 60 * (Math.pow(2, level - 1))
+    return 25 * (level ^ 2) + (50 * level) + 100
 }
 
 export function progressBar(currentXP: number, requiredXP: number): string {
