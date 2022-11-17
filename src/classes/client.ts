cc8985c590812c376f6b99115e456cf1c53f3488
raluvy95 <ralucadoge069@gmail.com>
2022-11-17 22:48:37 +0200

    Bump Jolly to v0.3.0

--- src/classes/client.ts
+++ src/classes/client.ts
@@ -2,7 +2,7 @@ import { config, createBot, CreateBotOptions, enableAudioPlugin, enableCachePlug
 import { JollyEvent } from "@classes/events.ts";
 import { main } from "@utils/log.ts";
 
-export const JollyVersion = "0.2.2"
+export const JollyVersion = "0.3.0"
 
 export class Jolly implements CreateBotOptions {
 
