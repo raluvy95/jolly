e70e48184d43844fc115bc61fedb1e997ad287fe
raluvy95 <ralucadoge069@gmail.com>
2022-11-19 12:34:23 +0200

    Updated deps to use fork of rss

--- deps.ts
+++ deps.ts
@@ -8,8 +8,10 @@ export * from "https://deno.land/x/sqlite@v3.4.0/mod.ts";
 export { enableAudioPlugin } from "https://raw.githubusercontent.com/raluvy95/discordeno-audio-plugin/main/mod.ts";
 export type { AudioBot } from "https://raw.githubusercontent.com/raluvy95/discordeno-audio-plugin/main/mod.ts";
 
+// fork of rss to fix breaking the bot
+export { parseFeed } from "https://raw.githubusercontent.com/raluvy95/rss/master/mod.ts";
+
 // local lib
 // export { enableAudioPlugin } from "../discordeno-audio-plugin/mod.ts";
 // export type { AudioBot } from "../discordeno-audio-plugin/mod.ts";
-
-export { parseFeed } from "https://deno.land/x/rss@0.5.6/mod.ts";
+// export { parseFeed } from "../rss/mod.ts";
