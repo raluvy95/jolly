475b4edbd44268771fe79c24519c6de252020b2a
raluvy95 <ralucadoge069@gmail.com>
2022-12-01 13:04:56 +0200

    better to escape, so users could see raw message

--- src/cmd/tools/run.ts
+++ src/cmd/tools/run.ts
@@ -4,6 +4,7 @@ import { send } from "@utils/send.ts";
 import { TIO } from "../../../lib/min-tio.js";
 import { IDS } from "../../../lib/id.ts";
 import { JollyEmbed } from "@classes/embed.ts";
+import { escape } from "@utils/escapeTxt.ts"
 
 interface Unknown {
     [key: string]: string;
@@ -15,8 +16,8 @@ class Run extends JollyCommand {
     }
 
     override async run(message: Message, args: string[], client: BotWithCache<Bot>) {
-        const err = "Please input your code (requires to have formatted codeblock). This is how codeblock should look like: \n```js\nconsole.log(\"hello world\")\n```"
-        if (!args.length) return send(client, message.channelId, err)
+        const err = "Please input your code (requires to have formatted codeblock). This is how codeblock should look like: \n\`\`\`c-clang\n#include <stdio.h>\n\nint main() {\n    printf(\"hello world\")\n    return 0;\n}\n\`\`\`"
+        if (!args.length) return send(client, message.channelId, escape(err))
         if (args[0].toLowerCase().startsWith("lang")) {
             const e = new JollyEmbed()
                 .setTitle(`Avaliable languages`)
@@ -24,7 +25,7 @@ class Run extends JollyCommand {
                 .build()
             return send(client, message.channelId, e)
         }
-        if (args[0].indexOf("```") == -1) return send(client, message.channelId, err)
+        if (args[0].indexOf("```") == -1) return send(client, message.channelId, escape(err))
         let lang = args[0].split("\n")[0].replace("```", "")
         for (const key in IDS) {
             if (key.toLowerCase().includes(lang)) {
@@ -36,7 +37,7 @@ class Run extends JollyCommand {
         const code = code1.slice(0, code1.lastIndexOf("```"))
         const r = await TIO.run(code, '', lang)
         if (r[0].startsWith("The language")) {
-            return send(client, message.channelId, `Type \`${prefix}run lang\` to show list of languages avaliable`)
+            return send(client, message.channelId, `Type \`${prefix}run lang\` to show list of languages avaliable.` + escape("Sometimes your language needs specific compiler/runtime (such as \`c-gcc\` or \`python3-pypy\`). This is how it supposed to use \nsudo run \`\`\`<lang>\nprint(\"hello world\")\n\`\`\`"))
         }
         const codeblock = r[0]?.length > 0 && r.length == 3 ? `\`\`\`${r[0]}\`\`\`\n` : "No output\n"
         const error = r[1]?.startsWith("/home/runner/.code.tio") || r[1]?.startsWith(".code.tio") || r[1]?.includes(".code.tio") ? `\`\`\`\n${r[1].split("\n").slice(0, 5).join("\n")}\n\`\`\`` : ""
---
+++ src/utils/escapeTxt.ts
@@ -0,0 +1,3 @@
+export const escape = (text: string): string => {
+    return text.replace(/(\_|\*|\~|\`|\||\\|\<|\>|\:|\!)/g, "\\$1").replace(/@(everyone|here|[!&]?[0-9]{17,21})/g, "@\u200b\$1");
+}
