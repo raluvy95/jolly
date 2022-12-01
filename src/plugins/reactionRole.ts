c0641d70a90e05420fcf7de78e2238fdd665131b
raluvy95 <ralucadoge069@gmail.com>
2022-12-01 19:41:14 +0200

    Now supports custom emoji & added type method

--- src/plugins/reactionRole.ts
+++ src/plugins/reactionRole.ts
@@ -1,6 +1,14 @@
 import { Bot, BotWithCache, config } from "@deps";
 import { ReactionRmPayload } from "../interfaces/reactionpayload.ts";
 
+function getEmojiName(emoji: string) {
+    if (emoji.startsWith("<")) {
+        const [_, name, _id] = emoji.split(":")
+        emoji = name
+    }
+    return emoji
+}
+
 export async function reaction(client: BotWithCache<Bot>, payload: ReactionRmPayload, type: "add" | "rm") {
     if (payload.userId == client.id) return;
     const reactionRoleConf = config.plugins.reactionRole
@@ -8,14 +16,23 @@ export async function reaction(client: BotWithCache<Bot>, payload: ReactionRmPay
     if (reactionRoleConf.reactions.length < 1) return;
     const currentReaction = reactionRoleConf.reactions.find(m => BigInt(m.channelID) == payload.channelId && BigInt(m.messageID) == payload.messageId)
     if (!currentReaction) return;
-    const currentRole = currentReaction.roleEmojis.find(m => m.emoji == payload.emoji.name)
+    const currentRole = currentReaction.roleEmojis.find(m => getEmojiName(m.emoji) == payload.emoji.name)
     if (!currentRole) return;
     const member = client.members.get(payload.userId) || await client.helpers.getMember(config.guildID, payload.userId)
     if (!member) return;
     switch (type) {
         case "add": {
             if (member.roles.some(m => m == BigInt(currentRole.roleID))) return;
+            const already_exist = member.roles.some(m => currentReaction.roleEmojis.findIndex(n => BigInt(n.roleID) == m) != -1)
+            if (currentReaction.type == "one" && already_exist) {
+                const roleToTake = member.roles.find(m => currentReaction.roleEmojis.findIndex(n => BigInt(n.roleID) == m) != -1)
+                if (!roleToTake) throw new Error("what.")
+                await client.helpers.removeRole(config.guildID, payload.userId, roleToTake, "Reaction Role | Used single choice method")
+            }
             await client.helpers.addRole(config.guildID, payload.userId, currentRole.roleID, "Reaction Role")
+            if (currentReaction.removeReactionAfterTrigger) {
+                await client.helpers.deleteUserReaction(payload.channelId, payload.messageId, payload.userId, getEmojiName(currentRole.emoji))
+            }
             break
         }
         case "rm": {
