69c2f213c7f0c48d9d455167d5bd8434956da4c4
raluvy95 <ralucadoge069@gmail.com>
2022-12-01 13:43:10 +0200

    fixed uncaught error

--- src/cmd/fun/cat.ts
+++ src/cmd/fun/cat.ts
@@ -10,14 +10,21 @@ class Cat extends JollyCommand {
         })
     }
 
-    override run(message: Message, _args: string[], client: BotWithCache<Bot>): void {
-        fetch("https://aws.random.cat/meow").then(async r => {
-            const j = await r.json()
-            send(client, message.channelId, j.file)
-        })
-
+    override run(message: Message, _args: string[], client: BotWithCache<Bot>) {
+        const err = "There's something went wrong with cat. No cat for you :("
+        try {
+            fetch("https://aws.random.cat/meow").then(async r => {
+                try {
+                    const j = await r.json()
+                    send(client, message.channelId, j.file)
+                } catch {
+                    send(client, message.channelId, err)
+                }
+            })
+        } catch {
+            return send(client, message.channelId, err)
+        }
     }
-
 }
 
 addCommand(new Cat())
